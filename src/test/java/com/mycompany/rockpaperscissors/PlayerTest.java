c03c21a9371be3c2e5aec489254afea023b51c22
Angel Miralles <anmiralles@yahoo.es>
2016-02-07 11:10:16 +0100

    Add junit test for Player and its methods. Following TDD first we code the test, verify it fails and after that we implement the method.

---
+++ src/test/java/com/mycompany/rockpaperscissors/PlayerTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rockpaperscissors;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author angelmiralles
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+
+    /**
+     * Test of getRandomChoice method, of class Player.
+     */
+    @Test
+    public void testGetRandomChoice() {
+    }
+
+    /**
+     * Test of getSelectedChoice method, of class Player.
+     */
+    @Test
+    public void testGetSelectedChoice() {
+    }
+    
+}
